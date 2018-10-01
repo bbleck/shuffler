e66a88982e9622f2bd16f0f804cf6914ee686d3a
brianb <bbleck@unm.edu>
2018-10-01 10:58:00 -0600

    Cleaned up commented lines

--- src/edu/cnm/deepdive/Shuffler.java
+++ src/edu/cnm/deepdive/Shuffler.java
@@ -1,24 +1,17 @@
 package edu.cnm.deepdive;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Shuffler {
 
   public static final int DECK_SIZE = 52;
-  private ArrayList<Integer> deckOne = new ArrayList<Integer>(DECK_SIZE);
   private int[] deckThree = new int[DECK_SIZE];
-  private ArrayList<Integer> deckTwo = new ArrayList<Integer>(0);
 
   public static void main(String[] args) {
     Shuffler theShuffler = new Shuffler();
     theShuffler.fillThirdDeck();
     theShuffler.shuffleDeckInPlace(DECK_SIZE - 1);
     theShuffler.printDeckThree();
-//    theShuffler.setUp();
-//    theShuffler.printDecks();
-//    theShuffler.shuffleDeck();
-//    theShuffler.printDecks();
   }
 
 
@@ -125,12 +118,6 @@ public class Shuffler {
     }
   }
 
-  private void fillOneDeck() {
-    for (int i = 0; i < DECK_SIZE; i++) {
-      deckOne.set(i, i + 1);
-    }
-  }
-
   private void shuffleDeckInPlace(int position) {
     if (position > 1) {
       swap(position);
@@ -144,42 +131,8 @@ public class Shuffler {
   private void swap(int position) {
     Random rng = new Random();
     int secondPosition = rng.nextInt(position);
-//    System.out.println(position);
-//    System.out.println(secondPosition);
     int tempPosVal = deckThree[position];
     deckThree[position] = deckThree[secondPosition];
     deckThree[secondPosition] = tempPosVal;
-//    deckOne.set(position, secondPosition+1);
-//    deckOne.set(secondPosition, position+1);
-  }
-
-  private void printDecks() {
-    System.out.println();
-    System.out.println("Deck 1:");
-    for (int i = 0; i < deckOne.size(); i++) {
-      System.out.println(deckOne.get(i).toString());
-    }
-    System.out.println("Deck 2:");
-    for (int i = 0; i < deckTwo.size(); i++) {
-      System.out.println(deckTwo.get(i).toString());
-    }
-  }
-
-  private void setUp() {
-    for (int i = 0; i < 52; i++) {
-      deckOne.add(i + 1);
-//      System.out.println(i+1);
-    }
-  }
-
-  void shuffleDeck() {
-    Random rng = new Random();
-    int card = rng.nextInt() * deckOne.size();
-    if (deckOne.size() > 0) {
-      deckTwo.add(deckOne.remove(card));
-      shuffleDeck();
-    } else {
-      return;
-    }
   }
 }
